45703a0406ee812e7630190afc13bfd60330f369
TacoCholoLocoYolo <51718543+TacoCholoLocoYolo@users.noreply.github.com>
2024-02-23 09:36:11 -0600

    Add files via upload

---
+++ CopyLocknWall.ps1
@@ -0,0 +1,7 @@
+﻿$url = "https://inprocorp.sharepoint.com/sites/sps_intune/shared%20documents/w11%20wallpapers/wall.jpg"
+$outputPath = "C:\Windows\wall.jpg"
+Invoke-WebRequest -Uri $url -OutFile $outputPath
+
+$url = "https://inprocorp.sharepoint.com/sites/sps_intune/shared%20documents/w11%20wallpapers/lock.jpg"
+$outputPath = "C:\Windows\lock.jpg"
+Invoke-WebRequest -Uri $url -OutFile $outputPath
